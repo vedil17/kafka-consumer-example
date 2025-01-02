719e582a7e3484216324620657fc1a3b7559a5e5
adil alouan <62723653+vedil17@users.noreply.github.com>
2025-01-02 12:21:48 +0100

    create consumer and consumer group

--- src/main/java/com/adilAlouan/consumer/KafkaMessageListener.java
+++ src/main/java/com/adilAlouan/consumer/KafkaMessageListener.java
@@ -1,4 +1,17 @@
 package com.adilAlouan.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaMessageListener {
+
+    Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+    @KafkaListener(topics = "topic2",groupId = "test-group")
+    public void consume(String message) {
+        logger.info("Consumer consume the message {}",message);
+    }
 }
